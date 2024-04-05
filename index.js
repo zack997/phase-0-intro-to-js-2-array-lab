234ad2cfa7f20a54a1173c8507b4fdde59f7d6d4
zacknjenga <izackkiarie@gmail.com>
2024-04-05 05:28:29 +0300

    adding intro js array

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+// Define the cats variable with an initial value
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Now you can use the cats variable in your code
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+ 
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+ 
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function appendCat(name) {
+    return cats.concat(name);
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
